804ba800fa7013d17df7b3e870e0672a8646659b
Emma Johnson <64632652+emmarubyjohnson@users.noreply.github.com>
2021-09-21 18:20:37 +1000

    Classwork

--- IGB321FinalLevel/Assets/Scripts/Chest.cs
+++ IGB321FinalLevel/Assets/Scripts/Chest.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +6,21 @@ public class Chest : MonoBehaviour
 {
     public GameManager gameManager;
 
+   // public GameObject door;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.instance;
     }
 
-    private void OnTriggerEnter(Collider collider)
+    private void OnTriggerStay(Collider collider)
     {
         if (collider.tag == "Player" && Input.GetKey("space")) 
         {
             this.GetComponent<Collider>().enabled = false;
+
+           // door.getComponent<MeshRenderer>()
             gameManager.keys += 1;
         }
     }
